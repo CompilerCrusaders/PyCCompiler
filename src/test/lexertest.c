04cab8879a9f75bf6749ecdd554f5567010a0d09
roanmorgan53 <roanlm007@gmail.com>
2023-10-11 20:17:17 -0700

    basic setup

---
+++ src/test/lexertest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+
+    int a = 5;
+    int b = 6;
+    int c = a + b;
+    c = c++;
+
+    char s[] = "can you read me?";
+
+    return 0;
+}
