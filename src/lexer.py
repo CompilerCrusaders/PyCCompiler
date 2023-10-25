ca0ce0c5018250bcdb5477f5b448ca36ee0dc0fd
roanmorgan53 <roanlm007@gmail.com>
2023-10-24 20:07:09 -0700

    comment edits

--- src/lexer.py
+++ src/lexer.py
@@ -1,20 +1,10 @@
 #Lexer to do list
-# 1. Read the input file
-# 2. Remove all the comments
-# 3. Remove all the white spaces
+# 1. [X] Read the input file
+# 2. [X] Remove all the comments
+# 3. [X] Remove all the white spaces
 # 4. Identify all the tokens
 # 5. Return the tokens
 # 6. Return the errors if any
-
-#token types
-# 1. symbols
-# 2. numbers
-# 3. = - + * / %
-# 4. () {} [] ; , .
-# 5. individuals variable types
-# 6. loops
-# 15. switch
-# 16. comments
 
 import sys
 import os
