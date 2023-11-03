bfe11a4584ac79776e230ce200cbb37ffa1d62f0
IShaLinI <mrsaltinecracker1@gmail.com>
2023-11-02 19:28:37 -0700

    new files

---
+++ src/test/cSample.c
@@ -0,0 +1,16 @@
+#include <stdbool.h> 
+
+int main(void){
+// This file is meant for testing purposes(and to test for comments)
+    int         x = 3;
+    int y = 4   ;
+    int    z =     x + y; //testing for whitespace
+
+    double w = 3.4;
+
+    bool test = true;
+
+    char singleChar = 'g';
+
+    return(0);
+}
